b1b357f7b3671d649f3d946831a982c286c0b8df
Igor Korostil <eeghor@gmail.com>
2017-12-01 00:57:52 +1100

    added basic setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tnormaliser',
+      version='0.1',
+      description='Normalise Transaction Descriptions',
+      url='https://github.com/i9k/tnormaliser',
+      author='Igor Korostil',
+      author_email='eeghor@gmail.com',
+      license='MIT',
+      packages=['tnormaliser'],
+      zip_safe=False)
