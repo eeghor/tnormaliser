d45c1382b2b07dc9baf711aaf92a216444573841
Igor Korostil <eeghor@gmail.com>
2017-12-01 10:16:21 +1100

    added readme

---
+++ README.md
@@ -0,0 +1,2 @@
+## Transaction Description Normaliser
+The key purpose of this library is to take care of string normalisation.
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tnormaliser',
-      version='0.1',
+      version='0.0.1',
       description='Normalise Transaction Descriptions',
       url='https://github.com/i9k/tnormaliser',
       author='Igor Korostil',
